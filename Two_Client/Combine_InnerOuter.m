773f146951e3183ae06dbd3732d683319d2c8572
mohadesehazari98 <84409349+mohadesehazari98@users.noreply.github.com>
2023-12-06 11:11:46 +0330

    comments added

--- Two_Client/Combine_InnerOuter.m
+++ Two_Client/Combine_InnerOuter.m
@@ -2,6 +2,7 @@ function [Rate] = Combine_InnerOuter(N, K_each, L, sigGKP, etad, ErrProb3Sigma, 
     ZerrInner_1, XerrInner_1, sTotal_1,...
     ZerrInner_2, XerrInner_2, sTotal_2)
 %--------------------------------------------------------------------------
+% This function takes Outer Leaves analog information, combined with Inner Leaves Error, and calculates the 6State protocol E2E Rate
 k_main = min(K_each);
 k_1=K_each(1);k_2=K_each(2);
 l_1=L(1);l_2=L(2);
@@ -12,8 +13,8 @@ XerrInnerMat_1 = XerrInner_1 .* ones(k_1,1);
 
 ZerrInnerMat_2 = ZerrInner_2 .* ones(k_2,1);
 XerrInnerMat_2 = XerrInner_2 .* ones(k_2,1);
-
 %--------------------------------------------------------------------------
+% Outer Leaves information coming from the left channel
 [ZerrOuter_1, XerrOuter_1] = Outer_Sim(N, l_1, sigGKP, etad, k_1, ErrProb3Sigma(1), ErrProb2Sigma(1));
 
 Zerr_1 = ZerrInnerMat_1.*(ones(k_1,1) - ZerrOuter_1) + ZerrOuter_1.*(ones(k_1,1) - ZerrInnerMat_1);
@@ -21,6 +22,7 @@ Xerr_1 = XerrInnerMat_1.*(ones(k_1,1) - XerrOuter_1) + XerrOuter_1.*(ones(k_1,1)
 
 clear ZerrOuter_1 ZerrInnerMat_1 XerrOuter_1 XerrInnerMat_1
 %--------------------------------------------------------------------------
+% Outer Leaves information coming from the right channel
 [ZerrOuter_2, XerrOuter_2] = Outer_Sim(N, l_2, sigGKP, etad, k_2, ErrProb3Sigma(2), ErrProb2Sigma(2));
 
 Zerr_2 = ZerrInnerMat_2.*(ones(k_2,1) - ZerrOuter_2) + ZerrOuter_2.*(ones(k_2,1) - ZerrInnerMat_2);
@@ -28,6 +30,7 @@ Xerr_2 = XerrInnerMat_2.*(ones(k_2,1) - XerrOuter_2) + XerrOuter_2.*(ones(k_2,1)
 
 clear ZerrOuter_2 ZerrInnerMat_2 XerrOuter_2 XerrInnerMat_2
 %--------------------------------------------------------------------------
+% Calculating the E2E rate while the resource states error are different 
 Rate = SecretKey6StateEndToEnd(...
     Zerr_1,...
     Xerr_1,...
